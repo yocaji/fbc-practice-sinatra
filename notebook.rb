7b19b830773c23e2fbba58463f6137b0eb810796
yocajii <yocajii@stone.sakura.ne.jp>
2022-03-23 10:38:46 +0900

    変数の渡し方とSQL文の渡し方を修正した
    - erbへ変数を渡す方法をテンプレート変数に変更
    - SQLの実行処理を1つのメソッドにまとめた

--- notebook.rb
+++ notebook.rb
@@ -9,23 +9,34 @@ class Notebook
   end
 
   def all
-    @conn.exec('select * from notes').to_a
+    query = 'select * from notes'
+    execute(query).to_a
   end
 
   def pick_note(id)
-    @conn.exec("select * from notes where id = #{id}").to_a[0]
+    query = "select * from notes where id = #{id}"
+    execute(query).to_a[0]
   end
 
   def add_note(title:, text:)
-    result = @conn.exec("insert into notes (title, text) values ('#{title}', '#{text}') returning id").to_a[0]
+    query = "insert into notes (title, text) values ('#{title}', '#{text}') returning id"
+    result = execute(query).to_a[0]
     result[:id]
   end
 
   def update_note(id:, title:, text:)
-    @conn.exec("update notes set title = '#{title}', text = '#{text}' where id = #{id}")
+    query = "update notes set title = '#{title}', text = '#{text}' where id = #{id}"
+    execute(query)
   end
 
   def remove_note(id)
-    @conn.exec("delete from notes where id = #{id}")
+    query = "delete from notes where id = #{id}"
+    execute(query)
+  end
+
+  private
+
+  def execute(query)
+    @conn.exec(query)
   end
 end
